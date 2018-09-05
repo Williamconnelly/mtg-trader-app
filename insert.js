265a890d7e3e0d9c9d025cd5d9c94937af1c7313
William Connelly <williamconnelly.dev@gmail.com>
2018-09-05 13:29:05 -0700

    Uncommented insert script to be functional

--- insert.js
+++ insert.js
@@ -7,36 +7,35 @@ const fs = require('fs');
 const cardFields = ["layout","name","names","manaCost","cmc","colors","colorIdentity",
 "type","supertypes","types","subtypes","rarity","text","power","toughness","loyalty"]
 
-const longArray = [];
-
-// for (card in dataSet) {
-//   let currentCard = dataSet[card]
-//   let insertCard = {};
-//   for (field in cardFields) {
-//     let property = cardFields[field]
-//     currentCard.hasOwnProperty(property) ? insertCard[property] = currentCard[property] : insertCard[property] = null;
-//   }
-//   db.card.create({
-//     layout: insertCard.layout,
-//     name: insertCard.name,
-//     names: insertCard.names,
-//     mana_cost: insertCard.manaCost,
-//     cmc: insertCard.cmc,
-//     colors: insertCard.colors,
-//     color_identity: insertCard.colorIdentity,
-//     type: insertCard.type,
-//     supertypes: insertCard.supertypes,
-//     types: insertCard.types,
-//     subtypes: insertCard.subtypes,
-//     rarity: insertCard.rarity,
-//     text: insertCard.text,
-//     power: insertCard.power,
-//     toughness: insertCard.toughness,
-//     loyalty: insertCard.loyalty
-//   })
-// }
+for (card in dataSet) {
+  let currentCard = dataSet[card]
+  let insertCard = {};
+  for (field in cardFields) {
+    let property = cardFields[field]
+    currentCard.hasOwnProperty(property) ? insertCard[property] = currentCard[property] : insertCard[property] = null;
+  }
+  db.card.create({
+    layout: insertCard.layout,
+    name: insertCard.name,
+    names: insertCard.names,
+    mana_cost: insertCard.manaCost,
+    cmc: insertCard.cmc,
+    colors: insertCard.colors,
+    color_identity: insertCard.colorIdentity,
+    type: insertCard.type,
+    supertypes: insertCard.supertypes,
+    types: insertCard.types,
+    subtypes: insertCard.subtypes,
+    rarity: insertCard.rarity,
+    text: insertCard.text,
+    power: insertCard.power,
+    toughness: insertCard.toughness,
+    loyalty: insertCard.loyalty
+  })
+}
 
 // Finds cards with a text length of greater than the allowed 400
+// const longArray = [];
 // for (card in dataSet) {
 //   let currentCard = dataSet[card];
 //   if (currentCard.hasOwnProperty("text") && currentCard.text.length > 400) {
