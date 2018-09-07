e22a15b5a7533d14fea867e5a0216d10975198e6
William Connelly <williamconnelly.dev@gmail.com>
2018-09-07 16:53:18 -0700

    Updated insertSet

--- insertSets.js
+++ insertSets.js
@@ -2,13 +2,38 @@ var express = require('express');
 var db = require("./models");
 const fs = require('fs');
 
-dataSet = 
+// dataSet = {};
 
-for (set in dataSet) {
-  if (dataSet[set].name !== "Unstable" || dataSet[set] !== "Unhinged" || dataSet[set].name !== "Unglued") {
-    db.set.create({
-      title: dataSet[set].name,
-      code: dataSet[set].code
-    })
-  }
+// Add Set titles and codes to db
+
+// for (set in dataSet) {
+//   if !(dataSet[set].name === "Unstable" || dataSet[set] === "Unhinged" || dataSet[set].name === "Unglued") {
+//     db.set.create({
+//       title: dataSet[set].name,
+//       code: dataSet[set].code
+//     })
+//   }
+// }
+
+// Create Relationships
+
+dataSet = {};
+
+for (card in dataSet) {
+  let currentCard = dataSet[card];
+  db.card.find({
+    where: {
+      name: currentCard.name
+    }
+  }).then(card => {
+    for (printing in currentCard.printings) {
+      db.set.find({
+        where: {
+          code: currentCard.printings[printing]
+        }
+      }).then(set => {
+        card.addSet(set);
+      })
+    }
+  })
 }
