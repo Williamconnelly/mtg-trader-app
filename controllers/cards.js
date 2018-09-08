22016ba761f0e0a2ac5c6f03d626f6edd0c62730
William Connelly <williamconnelly.dev@gmail.com>
2018-09-07 17:58:49 -0700

    Added body-parser dependency and made temp root route

--- controllers/cards.js
+++ controllers/cards.js
@@ -4,6 +4,15 @@ var router = express.Router();
 require('dotenv').config();
 const axios = require("axios");
 
-
+// Finds a specific card by ID
+router.get("/:id", (req, res) => {
+  db.card.find({
+    where: {
+      id: req.params.id
+    }
+  }).then(card => {
+    res.json(card);
+  })
+})
 
 module.exports = router;
--- server.js
+++ server.js
@@ -4,13 +4,18 @@ const Sequelize = require('sequelize');
 const Op = Sequelize.Op;
 var db = require("./models");
 const axios = require("axios");
+const bp = require("body-parser");
 
 var app = express();
+app.use(bp.json());
 
-
+app.get("/", (req, res) => {
+  res.send("Hello!");
+})
 
 app.use('/card', require('./controllers/cards'));
 
-var server = app.listen(process.env.PORT || 3000);
+var port = process.env.PORT || 3000;
+var server = app.listen(port, () => console.log(`Server now running on ${port}`));
 
 module.exports = server;
