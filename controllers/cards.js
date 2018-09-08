afccc179c736ce46fd3749df314be422aaf5a5ff
William Connelly <williamconnelly.dev@gmail.com>
2018-09-07 17:15:10 -0700

    Added axios module, created controllers folder, and added cards.js

---
+++ controllers/cards.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var db = require("../models");
+var router = express.Router();
+require('dotenv').config();
+const axios = require("axios");
+
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -3,9 +3,14 @@ var express = require("express");
 const Sequelize = require('sequelize');
 const Op = Sequelize.Op;
 var db = require("./models");
+const axios = require("axios");
 
 var app = express();
 
 
 
+app.use('/card', require('./controllers/cards'));
+
 var server = app.listen(process.env.PORT || 3000);
+
+module.exports = server;
