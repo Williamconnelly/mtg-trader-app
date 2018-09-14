250988ca647c945a93093e3a343065ba16e58271
William Connelly <williamconnelly.dev@gmail.com>
2018-09-14 13:39:38 -0700

    Wrote middleware to check incoming requests for auth headers and to verify tokens

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,6 +6,30 @@ var db = require("../models");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 
+// Middleware to check incoming request for auth header and verify token
+const verifyToken = (req, res, next) => {
+  // If the req has no auth header, return Unauthorized
+  if (!req.headers.authorization) {
+    res.status(401).send("Unauthorized Request");
+  } else {
+    // If the req has an auth header, splice out the token
+    let token = req.headers.authorization.split(' ')[1];
+    // If the token is null, return Unauthorized
+    if (token === 'null') {
+      res.status(401).send("Unauthorized Request");
+    }
+    // Verify the token with the jwt secret key
+    let payload = jwt.verify(token, process.env.JWT_SECRET)
+    // If the verification doesn't return anything, return Unauthorized
+    if (!payload) {
+      res.status(401).send("Unauthorized Request");
+    }
+    // If the token verifies correctly, set userId in the req and call NEXT
+    req.userId = payload.subject;
+    next();
+  }
+}
+
 // Signup
 router.post("/signup", (req, res) => {
   // Check for email - If none, CREATE
