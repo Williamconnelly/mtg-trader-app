b245c27aec9f2cc803ee7d1fd50d560667b89adf
William Connelly <williamconnelly.dev@gmail.com>
2018-09-14 20:25:14 -0700

    Created middleware directory and started two routes in trade controller

--- controllers/trade.js
+++ controllers/trade.js
@@ -3,11 +3,17 @@ var db = require("../models");
 var router = express.Router();
 const axios = require("axios");
 const Sequelize = require('sequelize');
+const verifyToken = require("../middleware/verifyToken");
 require('dotenv').config();
 
-router.get("/partners", (req, res) => {
-  console.log("HIT");
-  res.sendStatus(200).send("Hello");
-})
+// Find trade partners based on user wishlist
+router.get("/partners/want", (req, res) => {
+  res.status(200).send("Trading!");
+});
+
+// Find trade partners based on user tradelist
+router.get("/partners/provide", (req, res) => {
+  res.status(200).send("Trading!");
+});
 
 module.exports = router;
---
+++ middleware/verifyToken.js
@@ -0,0 +1,25 @@
+// Middleware to check incoming request for auth header and verify token
+const verifyToken = (req, res, next) => {
+  // If the req has no auth header, return Unauthorized
+  if (!req.headers.authorization) {
+    res.status(401).send("Unauthorized Request");
+  } else {
+    // If the req has an auth header, splice out the token
+    let token = req.headers.authorization.split(' ')[1];
+    // If the token is null, return Unauthorized
+    if (token === 'null') {
+      res.status(401).send("Unauthorized Request");
+    }
+    // Verify the token with the jwt secret key
+    let payload = jwt.verify(token, process.env.JWT_SECRET)
+    // If the verification doesn't return anything, return Unauthorized
+    if (!payload) {
+      res.status(401).send("Unauthorized Request");
+    }
+    // If the token verifies correctly, set userId in the req and call NEXT
+    req.userId = payload.subject;
+    next();
+  }
+}
+
+module.exports = verifyToken;
