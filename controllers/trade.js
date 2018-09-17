dea9cc01335ed10bad70350585dbe918cb9ccdff
William Connelly <williamconnelly.dev@gmail.com>
2018-09-17 16:27:15 -0700

    Completed gathering/want route

--- controllers/trade.js
+++ controllers/trade.js
@@ -8,6 +8,7 @@ require('dotenv').config();
 
 // Find trade partners based on user wishlist
 router.get("/gathering/want", verifyToken ,(req, res) => {
+  let tradePartners = [];
   // Find current User and include their associated cards (Wishlist)
   db.user.findOne({
     where: {
@@ -18,9 +19,11 @@ router.get("/gathering/want", verifyToken ,(req, res) => {
     for (card in user.dataValues.cards) {
       // If the card does not have a preferred printing
       if (user.dataValues.cards[card].dataValues.wishlist.dataValues.pref_printing === null) {
+        console.log("+++ STARTING QUERY FOR NO-PREF +++");
+        var cardId = user.dataValues.cards[card].dataValues.wishlist.dataValues.cardId;
         db.cardsSets.findAll({
           where: {
-            cardId: user.dataValues.cards[card].dataValues.wishlist.dataValues.cardId
+            cardId
           },
           include: [db.user]
         }).then(printings => {
@@ -28,6 +31,7 @@ router.get("/gathering/want", verifyToken ,(req, res) => {
           for (printing in printings) {
             // If a printing has more than 0 owners
             if (printings[printing].dataValues.users.length > 0) {
+              // For each associated owner, find the card in their collection and check for trade
               for (user in printings[printing].dataValues.users) {
                 db.collection.findOne({
                   raw: true,
@@ -36,8 +40,15 @@ router.get("/gathering/want", verifyToken ,(req, res) => {
                     cardsSetId: printings[printing].dataValues.id
                   }
                 }).then(ownedPrinting => {
+                  // If they have listed a copy of the card for trade
                   if (ownedPrinting.trade_copies > 0) {
-                    console.log("FOUND A USER WILLING TO TRADE!");
+                    console.log("********** FOUND A USER WILLING TO TRADE! **********");
+                    tradePartners.push({
+                      user: ownedPrinting.userId,
+                      printing: ownedPrinting.cardsSetId,
+                      trade_copies: ownedPrinting.trade_copies,
+                      card: cardId
+                    })
                   }
                 })
               }
@@ -46,9 +57,48 @@ router.get("/gathering/want", verifyToken ,(req, res) => {
         })
       // If the card has a preferred printing
       } else {
-        console.log("what");
+        console.log("+++ STARTING QUERY FOR PREF-PRINTING +++");
+        var printingId = user.dataValues.cards[card].dataValues.wishlist.dataValues.pref_printing
+        db.cardsSets.findAll({
+          where: {
+            id: printingId
+          },
+          include: [db.user]
+        }).then(printings => {
+          // For each printing of the card
+          for (printing in printings) {
+            // If a printing has more than 0 owners
+            var currentPrintingId = printings[printing].dataValues.id
+            var currentCardId = printings[printing].dataValues.cardId
+            if (printings[printing].dataValues.users.length > 0) {
+              for (user in printings[printing].dataValues.users) {
+                db.collection.findOne({
+                  raw: true,
+                  where: {
+                    userId: printings[printing].dataValues.users[user].id,
+                    cardsSetId: currentPrintingId
+                  }
+                }).then(ownedPrinting => {
+                  if (ownedPrinting.trade_copies > 0) {
+                    console.log("FOUND A USER WILLING TO TRADE!");
+                    tradePartners.push({
+                      user: ownedPrinting.userId,
+                      printing: ownedPrinting.cardsSetId,
+                      trade_copies: ownedPrinting.trade_copies,
+                      card: currentCardId
+                    })
+                  }
+                })
+              }
+            }
+          }
+        })
       }
     }
+    // HANDLING ASYNC LOOP. TODO: Find scalable solution
+    setTimeout(() => {
+      res.send(tradePartners);
+    }, 100)
   })
 });
 
