206a17a3af2e7cf4e943c887503db03e3c5923d0
William Connelly <williamconnelly.dev@gmail.com>
2018-09-20 16:09:47 -0700

    Refactored query to include 'required'. Added front-end display

--- controllers/trade.js
+++ controllers/trade.js
@@ -6,109 +6,6 @@ const Sequelize = require('sequelize');
 const op = Sequelize.Op;
 const verifyToken = require("../middleware/verifyToken");
 require('dotenv').config();
-/*
-// Find trade partners based on user wishlist
-router.get("/gathering/want", verifyToken ,(req, res) => {
-  let tradePartners = [];
-  // Find current User and include their associated cards (Wishlist)
-  db.user.findOne({
-    where: {
-      id: req.user.id
-    }, include: [db.card]
-  }).then(user => {
-    // For each card in the user's wishlist, get the card's printings
-    for (card in user.dataValues.cards) {
-      // If the card does not have a preferred printing
-      if (user.dataValues.cards[card].dataValues.wishlist.dataValues.pref_printing === null) {
-        console.log("+++ STARTING QUERY FOR NO-PREF +++");
-        var cardId = user.dataValues.cards[card].dataValues.wishlist.dataValues.cardId;
-        db.cardsSets.findAll({
-          where: {
-            cardId
-          },
-          include: [db.user]
-        }).then(printings => {
-          // For each printing of the card, check for related owners
-          for (printing in printings) {
-            // If a printing has more than 0 owners
-            var currentCardId = printings[printing].dataValues.cardId
-            if (printings[printing].dataValues.users.length > 0) {
-              // For each associated owner, find the card in their collection and check for trade
-              for (user in printings[printing].dataValues.users) {
-                db.collection.findOne({
-                  raw: true,
-                  where: {
-                    userId: printings[printing].dataValues.users[user].id,
-                    cardsSetId: printings[printing].dataValues.id
-                  }
-                }).then(ownedPrinting => {
-                  // If they have listed a copy of the card for trade
-                  if (ownedPrinting.trade_copies > 0) {
-                    console.log("********** FOUND A USER WILLING TO TRADE! **********");
-                    console.log(ownedPrinting);
-                    tradePartners.push({
-                      user: ownedPrinting.userId,
-                      printing: ownedPrinting.cardsSetId,
-                      trade_copies: ownedPrinting.trade_copies,
-                      card: currentCardId
-                    })
-                  }
-                })
-              }
-            }
-          }
-        })
-      // If the card has a preferred printing
-      } else {
-        console.log("+++ STARTING QUERY FOR PREF-PRINTING +++");
-        var printingId = user.dataValues.cards[card].dataValues.wishlist.dataValues.pref_printing
-        db.cardsSets.findAll({
-          where: {
-            id: printingId
-          },
-          include: [db.user]
-        }).then(printings => {
-          // For each printing of the card
-          for (printing in printings) {
-            // If a printing has more than 0 owners
-            var currentPrintingId = printings[printing].dataValues.id
-            var currentCardId = printings[printing].dataValues.cardId
-            if (printings[printing].dataValues.users.length > 0) {
-              for (user in printings[printing].dataValues.users) {
-                db.collection.findOne({
-                  raw: true,
-                  where: {
-                    userId: printings[printing].dataValues.users[user].id,
-                    cardsSetId: currentPrintingId
-                  }
-                }).then(ownedPrinting => {
-                  if (ownedPrinting.trade_copies > 0) {
-                    console.log("FOUND A USER WILLING TO TRADE!");
-                    tradePartners.push({
-                      user: ownedPrinting.userId,
-                      printing: ownedPrinting.cardsSetId,
-                      trade_copies: ownedPrinting.trade_copies,
-                      card: currentCardId
-                    })
-                  }
-                })
-              }
-            }
-          }
-        })
-      }
-    }
-    // HANDLING ASYNC LOOP. TODO: Find scalable solution
-    setTimeout(() => {
-      // const tradePartners = [
-      //   {user: [{},{},{},{}]},
-      //   {user: [{},{},{},{}]}
-      // ]
-      res.send(tradePartners);
-    }, 100)
-  })
-});
-*/
 
 router.get("/gathering/want", verifyToken, (req, res) => {
   let tradePartners = {};
@@ -125,40 +22,38 @@ router.get("/gathering/want", verifyToken, (req, res) => {
         db.cardsSets.findAll({
           where: {
             cardId: currentCard.id
-          }, include: [db.user]
+          }, include: [db.user], required: true
         }).then(printings => {
           for (printing in printings) {
-            if (printings[printing].dataValues.users.length > 0) {
-              for (partner in printings[printing].dataValues.users) {
-                let currentUser = printings[printing].dataValues.users[partner].dataValues;
-                let currentPrinting = printings[printing].dataValues
-                db.collection.findOne({
-                  raw: true,
-                  where: {
-                    userId: currentUser.id,
-                    cardsSetId: currentPrinting.id,
-                    trade_copies: {
-                      [op.gt]: 0
-                    }
+            for (partner in printings[printing].dataValues.users) {
+              let currentUser = printings[printing].dataValues.users[partner].dataValues;
+              let currentPrinting = printings[printing].dataValues
+              db.collection.findOne({
+                raw: true,
+                where: {
+                  userId: currentUser.id,
+                  cardsSetId: currentPrinting.id,
+                  trade_copies: {
+                    [op.gt]: 0
                   }
-                }).then(ownedPrinting => {
-                  tradePartners.hasOwnProperty(currentUser.id) ? 
-                  tradePartners[currentUser.id].cards.push({
+                }
+              }).then(ownedPrinting => {
+                tradePartners.hasOwnProperty(currentUser.id) ? 
+                tradePartners[currentUser.id].cards.push({
+                  cardId: currentCard.id,
+                  cardName: currentCard.name,
+                  trade_copies: ownedPrinting.trade_copies
+                }) : 
+                tradePartners[currentUser.id] = {
+                  userId: currentUser.id,
+                  userName: currentUser.username,
+                  cards: [{
                     cardId: currentCard.id,
                     cardName: currentCard.name,
                     trade_copies: ownedPrinting.trade_copies
-                  }) : 
-                  tradePartners[currentUser.id] = {
-                    userId: currentUser.id,
-                    userName: currentUser.username,
-                    cards: [{
-                      cardId: currentCard.id,
-                      cardName: currentCard.name,
-                      trade_copies: ownedPrinting.trade_copies
-                    }]
-                  }
-                })
-              }
+                  }]
+                }
+              })
             }
           }
         })
