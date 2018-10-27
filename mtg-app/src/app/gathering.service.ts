39f78d47b30d9fecb4ec310c565d4af9f51133cb
William Connelly <williamconnelly.dev@gmail.com>
2018-10-26 18:36:59 -0700

    Updated want api route to acquire

--- mtg-app/src/app/gathering.service.ts
+++ mtg-app/src/app/gathering.service.ts
@@ -5,8 +5,10 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class GatheringService {
-  private _gatheringWantUrl = 'http://localhost:3000/trade/gathering/want';
+  private _gatheringWantUrl = 'http://localhost:3000/trade/gathering/acquire';
   private _gatheringProvideUrl = 'http://localhost:3000/trade/gathering/provide';
+  private _gatheringCardURL = 'http://localhost:3000/trade/gathering/card';
+  private _gatheringUserURL = 'http://localhost:3000/trade/gathering/user';
   constructor(private http: HttpClient) { }
 
   getGatheringWant() {
@@ -15,6 +17,12 @@ export class GatheringService {
 
   getGatheringProvide() {
     return this.http.get<any>(this._gatheringProvideUrl);
+  }
+  searchCard() {
+    return this.http.get<any>(this._gatheringCardURL);
+  }
+  searchUser() {
+    return this.http.get<any>(this._gatheringUserURL);
   }
 }
 
