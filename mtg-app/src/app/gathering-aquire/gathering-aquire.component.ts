50c954ab3d562e791668c409f035b8d18adda8fd
William Connelly <williamconnelly.dev@gmail.com>
2018-10-30 15:27:33 -0700

    Fixed bug with gathering card filter display

--- mtg-app/src/app/gathering-aquire/gathering-aquire.component.ts
+++ mtg-app/src/app/gathering-aquire/gathering-aquire.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'app-gathering-aquire',
   templateUrl: './gathering-aquire.component.html',
   styleUrls: ['./gathering-aquire.component.css']
 })
-export class GatheringAquireComponent implements OnInit {
+export class GatheringAquireComponent implements OnInit, OnChanges {
   @Input() data;
   @Input() Otherdata;
 
@@ -13,6 +13,9 @@ export class GatheringAquireComponent implements OnInit {
 
   ngOnInit() {
 
+  }
+  ngOnChanges(changes) {
+    console.log(changes);
   }
 
 }
--- mtg-app/src/app/gathering-table/gathering-table.component.ts
+++ mtg-app/src/app/gathering-table/gathering-table.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'app-gathering-table',
   templateUrl: './gathering-table.component.html',
   styleUrls: ['./gathering-table.component.css']
 })
-export class GatheringTableComponent implements OnInit {
+export class GatheringTableComponent implements OnInit, OnChanges {
   @Input() data;
   @Input() Otherdata;
   dataSource = [];
@@ -18,6 +18,10 @@ export class GatheringTableComponent implements OnInit {
   ngOnInit() {
     this.dataSource = this.data;
     this.dataSource2 = this.Otherdata;
+  }
+  ngOnChanges(changes) {
+    // console.log('CHANGES');
+    // console.log(changes);
   }
 
 }
