a0a53c92b87a2d2ad62ba0d5b8f7cd457aed07f4
William Connelly <williamconnelly.dev@gmail.com>
2018-10-30 15:27:33 -0700

    Fixed bug with gathering card filter display

--- mtg-app/src/app/gathering/gathering.component.ts
+++ mtg-app/src/app/gathering/gathering.component.ts
@@ -16,7 +16,8 @@ export class GatheringComponent implements OnInit {
     user: false
   };
   setOptions = [];
-  setSelection = '';
+  setSelection = 'all';
+  testVar = [];
 
   // asyncTabs: Observable<any>;
   gathering = [];
@@ -38,6 +39,7 @@ export class GatheringComponent implements OnInit {
     this._gatheringService.searchAcquireCard(this.cardSearch).subscribe(result => {
       console.log(result);
       this.gathering = result;
+      this.setOptions = [];
       for (let user in result) {
         for (let set in result[user].cards) {
           if (!this.setOptions.includes(result[user].cards[set]['card.cardPrintings.set.title'])) {
@@ -91,12 +93,38 @@ export class GatheringComponent implements OnInit {
     this.gather();
   }
   refineCardSearch() {
-    console.log('Refined!');
-    console.log(this.setSelection);
-    for (const user in this.gathering) {
-      const refinedCards = this.gathering[user].cards.filter(card => card['card.cardPrintings.set.title'] === this.setSelection);
-      this.gathering[user].cards = refinedCards;
-      this.gathering = this.gathering;
+    // console.log('Refined!');
+    // console.log(this.setSelection);
+    // for (const user in this.gathering) {
+    //   // const refinedCards = this.gathering[user].cards.filter(card => card['card.cardPrintings.set.title'] === this.setSelection);
+    //   // this.gathering[user].cards = refinedCards;
+    //   for (let card in this.gathering[user].cards) {
+    //     if (this.gathering[user].cards[card]['card.cardPrintings.set.title'] !== this.setSelection) {
+    //       this.gathering[user].cards.splice(this.gathering[user].cards.indexOf(this.gathering[user].cards[card]), 1);
+    //     }
+    //   }
+    //   console.log("BEFORE CUT" + this.gathering[user].username)
+    //   console.log(this.gathering);
+    //   if (this.gathering[user].cards.length < 1) {
+    //     this.gathering.splice(this.gathering.indexOf(this.gathering[user]), 1);
+    //   }
+    // }
+    // console.log("AFTER CUT");
+    // console.log(this.gathering);
+    this.testVar = new Array(this.gathering.length);
+    for (let i = this.gathering.length - 1; i >= 0; i --) {
+      this.testVar[i] = {username: this.gathering[i].username, userId: this.gathering[i].userId};
+      const refinedCards = this.gathering[i].cards.filter(card => card['card.cardPrintings.set.title'] === this.setSelection);
+      this.testVar[i].cards = refinedCards;
+      // for (let o = this.gathering[i].cards.length - 1; o >= 0; o --) {
+      //   if (this.gathering[i].cards[o]['card.cardPrintings.set.title'] !== this.setSelection) {
+      //     console.log(this.gathering[i].cards[o]['card.cardPrintings.set.title']);
+      //     console.log(this.gathering[i].cards.splice(this.gathering[i].cards.indexOf(this.gathering[i].cards[o]), 1));
+      //   }
+      // }
     }
+    this.gathering = this.testVar;
+    // this.gathering = this.gathering;
+    // console.log(this.gathering);
   }
 }
