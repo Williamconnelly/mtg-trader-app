caf5c4eafbd91329a89dd64deb1d38ee1dec7436
William Connelly <williamconnelly.dev@gmail.com>
2018-11-02 18:48:39 -0700

    Completed cardsearch filter

--- mtg-app/src/app/gathering-aquire/gathering-aquire.component.ts
+++ mtg-app/src/app/gathering-aquire/gathering-aquire.component.ts
@@ -1,21 +1,70 @@
-import { Component, OnInit, Input, OnChanges } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-gathering-aquire',
   templateUrl: './gathering-aquire.component.html',
   styleUrls: ['./gathering-aquire.component.css']
 })
-export class GatheringAquireComponent implements OnInit, OnChanges {
+export class GatheringAquireComponent implements OnInit {
   @Input() data;
   @Input() Otherdata;
+  @Input() refineAcquire;
+  @Input() refineProvide;
+  @Input() searchState;
+  // @Input() setSelection;
+  // @Input() foilSelection;
+  // @Input() cardNumberSelection;
+  setSelection = 'all';
+  foilSelection = 'all';
+  cardNumberSelection = 'all';
+  // @Input() setOptions;
+  // @Input() foilOptions;
+  // @Input() cardNumberOptions;
 
   constructor() { }
 
   ngOnInit() {
 
   }
-  ngOnChanges(changes) {
-    console.log(changes);
-  }
+  refineCardSearch() {
+    const refinedData = new Array(this.refineAcquire.preservedCardSearch.length);
+    for (let i = this.refineAcquire.preservedCardSearch.length - 1; i >= 0; i --) {
+      refinedData[i] = {username: this.refineAcquire.preservedCardSearch[i].username,
+         userId: this.refineAcquire.preservedCardSearch[i].userId};
+      const refinedCards = this.refineAcquire.preservedCardSearch[i].cards.filter(
+        card => (card['card.cardPrintings.set.title'] === this.setSelection ||
+                this.setSelection === 'all') &&
+                (card['card.cardPrintings.users.collection.foil'] === this.foilSelection ||
+                this.foilSelection === 'all') &&
 
+                (card['card.cardPrintings.users.collection.trade_copies'] >= this.cardNumberSelection ||
+                this.cardNumberSelection === 'all')
+      );
+      refinedData[i].cards = refinedCards;
+      if (refinedData[i].cards.length < 1) {
+        refinedData.splice(refinedData[i], 1);
+      }
+      this.data = refinedData;
+    }
+  }
+  refineCardSearchProvide() {
+    const refinedData = new Array(this.refineProvide.preservedCardSearch.length);
+    for (let i = this.refineProvide.preservedCardSearch.length - 1; i >= 0; i --) {
+      refinedData[i] = {username: this.refineProvide.preservedCardSearch[i].username,
+         userId: this.refineProvide.preservedCardSearch[i].userId};
+      const refinedCards = this.refineProvide.preservedCardSearch[i].cards.filter(
+        card => (card['printing.set.title'] === this.setSelection ||
+                this.setSelection === 'all') &&
+                (card['printing.card.users.wishlist.pref_foil'] === this.foilSelection ||
+                this.foilSelection === 'all') &&
+                (card['printing.card.users.wishlist.number_wanted'] <= this.cardNumberSelection ||
+                this.cardNumberSelection === 'all')
+      );
+      refinedData[i].cards = refinedCards;
+      if (refinedData[i].cards.length < 1) {
+        refinedData.splice(refinedData[i], 1);
+      }
+      this.Otherdata = refinedData;
+    }
+  }
 }
--- mtg-app/src/app/gathering/gathering.component.ts
+++ mtg-app/src/app/gathering/gathering.component.ts
@@ -11,18 +11,25 @@ import { AuthService} from '../auth.service';
 export class GatheringComponent implements OnInit {
   cardSearch = '';
   userSearch = '';
-  searchData = {
+  searchState = {
     card: false,
     user: false,
     searchName: ''
   };
-  setOptions = [];
-  foilOptions = [];
-  setSelection = 'all';
-  foilSelection = 'all';
-  preservedCardSearch = [];
-  gathering = [];
+  acquire = [];
   provide = [];
+  refineAcquire = {
+    preservedCardSearch: [],
+    setOptions: ['all'],
+    foilOptions: ['all'],
+    cardNumberOptions: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+  };
+  refineProvide = {
+    preservedCardSearch: [],
+    setOptions: ['all'],
+    foilOptions: ['all'],
+    cardNumberOptions: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+  };
   // asyncTabs: Observable<any>;
   constructor(private _gatheringService: GatheringService, private _authService: AuthService) {
 //     this.asyncTabs = Observable.create((observer: Observer<any>) => {
@@ -41,41 +48,74 @@ export class GatheringComponent implements OnInit {
     this._gatheringService.searchAcquireCard(cardString).subscribe(result => {
       console.log(result);
       // Update gathering to use the new data and duplicate the data into a separate component variable
-      this.preservedCardSearch = this.gathering = result;
-      this.setOptions = [];
-      this.foilOptions = [];
+      this.refineAcquire.preservedCardSearch = this.acquire = result;
+      this.refineAcquire.setOptions = [];
+      this.refineAcquire.foilOptions = [];
+      this.refineAcquire.cardNumberOptions = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+      const largestOffer = 10;
       for (let user in result) {
         for (let set in result[user].cards) {
-          if (!this.setOptions.includes(result[user].cards[set]['card.cardPrintings.set.title'])) {
-            this.setOptions.push(result[user].cards[set]['card.cardPrintings.set.title']);
+          if (!this.refineAcquire.setOptions.includes(result[user].cards[set]['card.cardPrintings.set.title'])) {
+            this.refineAcquire.setOptions.push(result[user].cards[set]['card.cardPrintings.set.title']);
           }
-          if (!this.foilOptions.includes(result[user].cards[set]['card.cardPrintings.users.collection.foil'])) {
-            this.foilOptions.push(result[user].cards[set]['card.cardPrintings.users.collection.foil']);
+          if (!this.refineAcquire.foilOptions.includes(result[user].cards[set]['card.cardPrintings.users.collection.foil'])) {
+            this.refineAcquire.foilOptions.push(result[user].cards[set]['card.cardPrintings.users.collection.foil']);
+          }
+          if (result[user].cards[set]['card.cardPrintings.users.collection.trade_copies'] > largestOffer) {
+            largestOffer = result[user].cards[set]['card.cardPrintings.users.collection.trade_copies'];
           }
         }
       }
-      console.log(this.setOptions);
-      console.log(this.foilOptions);
+      if (largestOffer > this.refineAcquire.cardNumberOptions[this.refineAcquire.cardNumberOptions.length - 1]) {
+        this.refineAcquire.cardNumberOptions.push(largestOffer);
+      }
+      console.log(this.refineAcquire.setOptions);
+      console.log(this.refineAcquire.foilOptions);
+      console.log(this.refineAcquire.cardNumberOptions);
     });
     this._gatheringService.searchProvideCard(this.cardSearch).subscribe(result => {
       console.log(result);
-      this.provide = result;
+      this.refineProvide.preservedCardSearch = this.provide = result;
+      this.refineProvide.setOptions = [];
+      this.refineProvide.foilOptions = [];
+      this.refineProvide.cardNumberOptions = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+      const largestOffer = 10;
+      for (let i = 0; i < result.length; i++) {
+        for (const card in result[i].cards) {
+          if (!this.refineProvide.setOptions.includes(result[i].cards[card]['printing.set.title'])) {
+            this.refineProvide.setOptions.push(result[i].cards[card]['printing.set.title']);
+          }
+          if (!this.refineProvide.foilOptions.includes(result[i].cards[card]['printing.card.users.wishlist.pref_foil'])) {
+            this.refineProvide.foilOptions.push(result[i].cards[card]['printing.card.users.wishlist.pref_foil']);
+          }
+          if (result[i].cards[card]['printing.card.users.wishlist.number_wanted: 1'] > largestOffer) {
+            largestOffer = result[i].cards[card]['printing.card.users.wishlist.number_wanted: 1'];
+          }
+        }
+        if (largestOffer > this.refineProvide.cardNumberOptions[this.refineProvide.cardNumberOptions.length - 1]) {
+          this.refineProvide.cardNumberOptions.push(largestOffer);
+        }
+      }
+      if (largestOffer > this.refineProvide.cardNumberOptions[this.refineProvide.cardNumberOptions.length - 1]) {
+        this.refineProvide.cardNumberOptions.push(largestOffer);
+      }
+      console.log(this.refineProvide);
     });
-    this.searchData.searchName = cardString;
-    this.searchData.card = true;
+    this.searchState.searchName = cardString;
+    this.searchState.card = true;
   }
   submitUserSearch(userString) {
     console.log(`Searching for ${this.userSearch}`);
     this._gatheringService.searchAcquireUser(userString).subscribe(result => {
       console.log(result);
-      this.gathering = result;
+      this.acquire = result;
     });
     this._gatheringService.searchProvideUser(userString).subscribe(result => {
       console.log(result);
       this.provide = result;
     });
-    this.searchData.searchName = userString;
-    this.searchData.user = true;
+    this.searchState.searchName = userString;
+    this.searchState.user = true;
   }
   gather() {
     this._gatheringService.getGatheringAcquire().subscribe(
@@ -83,8 +123,8 @@ export class GatheringComponent implements OnInit {
         if (res.hasOwnProperty('error')) {
           this._authService.logoutUser();
         } else {
-          this.gathering = res;
-          console.log(this.gathering);
+          this.acquire = res;
+          console.log(this.acquire);
         }
       },
       err => console.log(err)
@@ -98,50 +138,27 @@ export class GatheringComponent implements OnInit {
     );
   }
   reset() {
-    this.searchData.card = this.searchData.user = false;
+    this.searchState.card = this.searchState.user = false;
     this.cardSearch = this.userSearch = '';
     this.gather();
   }
-  refineCardSearch() {
-    if (this.setSelection === 'all') {
-      const refinedData = new Array(this.preservedCardSearch.length);
-      for (let i = this.preservedCardSearch.length - 1; i >= 0; i --) {
-        refinedData[i] = {username: this.preservedCardSearch[i].username, userId: this.preservedCardSearch[i].userId};
-        if (this.foilSelection !== 'all') {
-          const refinedCards = this.preservedCardSearch[i].cards.filter(
-            card => card['card.cardPrintings.users.collection.foil'] === this.foilSelection
-          );
-          refinedData[i].cards = refinedCards;
-          if (refinedData[i].cards.length < 1) {
-            refinedData.splice(refinedData[i], 1);
-          }
-          this.gathering = refinedData;
-        } else {
-          this.gathering = this.preservedCardSearch;
-        }
-      }
-    } else {
-      const refinedData = new Array(this.preservedCardSearch.length);
-      for (let i = this.preservedCardSearch.length - 1; i >= 0; i --) {
-        refinedData[i] = {username: this.preservedCardSearch[i].username, userId: this.preservedCardSearch[i].userId};
-        let refinedCards = [];
-        if (this.foilSelection !== 'all') {
-          refinedCards = this.preservedCardSearch[i].cards.filter(
-            card => card['card.cardPrintings.set.title'] === this.setSelection &&
-            card['card.cardPrintings.users.collection.foil'] === this.foilSelection
-          );
-        } else {
-          refinedCards = this.preservedCardSearch[i].cards.filter(card => card['card.cardPrintings.set.title'] === this.setSelection);
-        }
-        refinedData[i].cards = refinedCards;
-        if (refinedData[i].cards.length < 1) {
-          refinedData.splice(refinedData[i], 1);
-        }
-      }
-      this.gathering = refinedData;
-    }
-  }
-  refineByFoil() {
-
-  }
+  // refineCardSearch() {
+  //   const refinedData = new Array(this.preservedCardSearch.length);
+  //   for (let i = this.preservedCardSearch.length - 1; i >= 0; i --) {
+  //     refinedData[i] = {username: this.preservedCardSearch[i].username, userId: this.preservedCardSearch[i].userId};
+  //     const refinedCards = this.preservedCardSearch[i].cards.filter(
+  //       card => (card['card.cardPrintings.set.title'] === this.setSelection ||
+  //               this.setSelection === 'all') &&
+  //               (card['card.cardPrintings.users.collection.foil'] === this.foilSelection ||
+  //               this.foilSelection === 'all') &&
+  //               (card['card.cardPrintings.users.collection.trade_copies'] >= this.cardNumberSelection ||
+  //               this.cardNumberSelection === 'all')
+  //     );
+  //     refinedData[i].cards = refinedCards;
+  //     if (refinedData[i].cards.length < 1) {
+  //       refinedData.splice(refinedData[i], 1);
+  //     }
+  //     this.acquire = refinedData;
+  //   }
+  // }
 }
