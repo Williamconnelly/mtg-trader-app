04f472549f35dfa136a1b455b5bd8a5f904d69ac
William Connelly <williamconnelly.dev@gmail.com>
2018-11-07 17:21:40 -0800

    Created new trading components

---
+++ mtg-app/src/app/trading/trade-message/trade-message.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-trade-message',
+  templateUrl: './trade-message.component.html',
+  styleUrls: ['./trade-message.component.css']
+})
+export class TradeMessageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
